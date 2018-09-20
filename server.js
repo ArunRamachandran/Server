1ff60f6ac1aac54d769857dae66a1c7216392565
arun.ramachandran <Arun-k.Ramachandran@lloydsbanking.com>
2018-09-20 20:26:37 +0530

    Updated server config

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
 const express = requires('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+require('dotenv').config();
+
 const app = express();
 const port = process.env.PORT || 5000;
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json()); // enabling application to use JSON by using bodyParser.json() middleware
 
 //console.log that your server is up & running
 app.listen(port, () => console.log(`Listening on port ${port}`));
