7ec44983afa26109e1ab073521807fbe95edefe3
ArunRamachandran <myself.arunkr@gmail.com>
2018-09-23 23:56:34 +0530

    Added routes to handle api calls more efficiently. Resolved CORS issue

--- server.js
+++ server.js
@@ -1,18 +1,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const MongoClient = require('mongodb').MongoClient;
 require('dotenv').config();
+
+const visitors =  require('./routes/visitor');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json()); // enabling application to use JSON by using bodyParser.json() middleware
 
 //console.log that your server is up & running
 app.listen(port, () => console.log(`Listening on port ${port}`));
 
-// create a GET route
-app.get('/test', (req, res) => {
-    res.send({ express: 'Rays of hope .. :) ' });
-})
+app.use('/api/test', visitors);
