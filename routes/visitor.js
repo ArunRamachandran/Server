2777d6b8864a1b6d14ad595508ffd49f2c3334a2
ArunRamachandran <myself.arunkr@gmail.com>
2018-09-29 22:16:15 +0530

    Added dummy apis to find user details & visitor time out

--- routes/visitor.js
+++ routes/visitor.js
@@ -5,4 +5,12 @@ router.get('/', (req, res) => {
   res.send({status: true, response: 'Hello node'});
 })
 
+router.get('/findUser', (req, res) => {
+  res.send({status: true, response: 'Believe'});
+})
+
+router.get('/outTime', (req, res) => {
+  res.send({status: true, response: 'OUT time recorded'});
+})
+
 module.exports = router;
